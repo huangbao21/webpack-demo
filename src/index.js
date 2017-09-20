9cc096601864b8d71eaaef4f629a63df60bb3fe0
BJ-HUANG <420749518@qq.com>
2017-09-20 17:36:42 +0800

    assent management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './style.css';
+import naruto from './naruto.jpg'
 
 function component(){
 	var element = document.createElement("div");
 
 	element.innerHTML = _.join(['Hello','webpack'],' ');
+	element.classList.add("hello");
+
+	var img = new Image();
+	img.src = naruto;
+	element.appendChild(img);
 
 	return element;
 }
