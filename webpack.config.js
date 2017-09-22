ffa6b5315ca20130dc82ef61c99e68b013778f0d
BJ-HUANG <420749518@qq.com>
2017-09-22 17:11:48 +0800

    development process

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
 		app: './src/index.js',
 		print: './src/print.js',
 	},
+	devtool: 'inline-source-map',
+	devServer:{
+		contentBase: './dist'
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'output Management'
