f07de66e339cd58fb5b94b7904e439b9a9bfe0fe
BJ-HUANG <420749518@qq.com>
2017-09-26 16:10:40 +0800

    have done code splitting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import {cube} from './math.js';
 import printMe from './print.js';
 import './style.css'
+
+if(process.env.NODE_ENV !== 'production'){
+	console.log("looks like we are in development mode");
+}
+
 function component(){
-	var element = document.createElement("div");
+	var element = document.createElement("pre");
 	var btn = document.createElement('buttton');
 
-	element.innerHTML = _.join(['Hello','webpack'],' ');
+	element.innerHTML = [
+		'Hello webpack',
+		'5 cubed in equle to '+ cube(5)
+	].join('\n\n')
 
 	btn.innerHTML = ' Click me and check the console';
 	btn.onclick = printMe;
@@ -14,7 +23,7 @@ function component(){
 
 	return element;
 }
-let element = component();
+var element = component();
 document.body.appendChild(element);
 
 if(module.hot){
--- webpack.common.js
+++ webpack.common.js
@@ -2,26 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin'); 
 const webpack = require('webpack');
+
 module.exports = {
 	entry: {
 		app: './src/index.js',
 	},
-	devtool: 'inline-source-map',
-	devServer:{
-		contentBase: './dist',
-		hot: true
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Hot Module Replacement'
-		}),
-		new CleanWebpackPlugin(['dist']),
-		new webpack.HotModuleReplacementPlugin(),
-	],
+	
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Production'
+		}),
+		new CleanWebpackPlugin(['dist']),
+		new webpack.optimize.CommonsChunkPlugin({
+			name:'common'
+		})
+	],
 	module:{
 		rules:[
 			{
